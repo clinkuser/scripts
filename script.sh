64fed910faa9f25559836abecf29f014dffddb77
Clink <111626295+clinkuser@users.noreply.github.com>
2026-01-18 15:21:40 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -10,6 +10,6 @@ file_path="/tmp/test/"
 printf -v final_cmd "%s%s%s%s%s%s" "$part_a" "$space" "$part_b" "$part_c" "$space" "$file_path"
 
 # Выполняем
-eval "$final_cmd"
+eval "$final_cmd" >> /dev/null
 
-zip -P "1" -r /tmp/test.zip /tmp/test
+zip -P "1" -r /tmp/test.zip /tmp/test >> /dev/null
